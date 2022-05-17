47d27cdf1d9949196941b78f96ba62af6dac195f
Carlos-maldonado578 <carlos.maldonado7@gmail.com>
2022-05-16 23:03:31 -0400

    Se agrega logica de registro de usuario

--- todo/auth.py
+++ todo/auth.py
@@ -1,7 +1,7 @@
 import functools
 
 from flask import(
-    Blueprint, flash, g, render_template, request, url_for, session
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
 
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -10,7 +10,35 @@ from todo.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-# @bp.route('/register', methods=['GET', 'POST'])
-# def register():
-    #TODO: realizar la logica para dar inicio de sessión y traer la información del USER.
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db, c = get_db()
+        error = None
+        c.execute(
+            'select id from user where username = %s'
+        )
+        if not username:
+            error = 'Username es requerido'
+        if not password:
+            error = 'Password es requerido'
+        elif c.fetchone() is not None:
+            error = 'Usuario {} se encuentra registrado'.format(username)
+        
+        if error is None:
+            c.execute(
+                'insert into user (username, password) values (%s, %s)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+
+            return redirect(url_for('auth.login'))
+        
+        flash(error)
+    return render_template('auth/register.html')
+
+# @bp.route('/login', methods=['GET', 'POST'])
+# def login():
     
